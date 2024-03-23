79330aa9a76e670c5b73d414f6d3e3c02a2e5a77
jhonnybrob <jakecapacite90@gmail.com>
2024-03-23 16:20:40 +0800

    first commit

---
+++ src/config/dbConnector.java
@@ -0,0 +1,30 @@
+
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class dbConnector {
+    
+    private Connection connect;
+      
+     public dbConnector(){
+            try{
+                connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/sampledb1", "root", "");
+            }catch(SQLException ex){
+                    System.out.println("Can't connect to database: "+ex.getMessage());
+            }
+        }
+
+        public ResultSet getData(String sql) throws SQLException{
+            Statement stmt = connect.createStatement();
+            ResultSet rst = stmt.executeQuery(sql);
+            return rst;
+        }
+     
+     
+}
